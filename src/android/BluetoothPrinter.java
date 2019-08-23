d31caa1d88bd3fedf155941da6d21c795ef30b87
EDcasa <dcasa@tsa.ec>
2019-08-23 13:10:10 -0500

    chage with functionalities of bixolon sdk

--- src/android/BluetoothPrinter.java
+++ src/android/BluetoothPrinter.java
@@ -172,7 +172,7 @@ public class BluetoothPrinter extends CordovaPlugin {
             posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT, "holamundo" + "\n");
             callbackContext.success("Data Sent");
           } catch (JposException e) {
-            callbackContext.error("Data Not send");
+            callbackContext.error("Data Not send" + e.toString());
             e.printStackTrace();
           } finally {
             try {
