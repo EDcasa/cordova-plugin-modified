c10d5b521ee5838ff6c30ac0e6c5775ee804482b
EDcasa <dcasa@tsa.ec>
2019-08-26 10:25:52 -0500

    Add print image functionality

--- src/android/BluetoothPrinter.java
+++ src/android/BluetoothPrinter.java
@@ -646,7 +646,7 @@ public class BluetoothPrinter extends CordovaPlugin {
         bxlConfigLoader = new BXLConfigLoader(context);
         try {
           bxlConfigLoader.openFile();
-          callbackContext.success("Data Sent start");
+          //callbackContext.success("Data Sent start");
         } catch (Exception e) {
           //callbackContext.error("Data Sent eror", e.getMessage().toString());
           e.printStackTrace();
@@ -799,7 +799,7 @@ public class BluetoothPrinter extends CordovaPlugin {
               buffer.put((byte) 0x00);
               Log.v("inputstream", "buffer");
               posPrinter.printBitmap(buffer.getInt(0), path, posPrinter.getRecLineWidth(), POSPrinterConst.PTR_BM_LEFT);
-    
+              callbackContext.success("Activity","print");
             } catch (JposException e) {
               e.printStackTrace();
               Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
